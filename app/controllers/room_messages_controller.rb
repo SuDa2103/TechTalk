f6114e3580e1f2596565ec300c6e81e79f1a65cc
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-01-30 20:02:52 +0000

    first attempt at adding action cable

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,10 +1,12 @@
 class RoomMessagesController < ApplicationController
- before_action :load_entities
+before_action :load_entities
 
   def create
     @room_message = RoomMessage.create user: current_user,
-                                       room: @room,
-                                       message: params.dig(:room_message, :message)
+                                      room: @room,
+                                      message: params.dig(:room_message, :message)
+
+    RoomChannel.broadcast_to @room, @room_message
   end
 
   protected
@@ -13,4 +15,3 @@ class RoomMessagesController < ApplicationController
     @room = Room.find params.dig(:room_message, :room_id)
   end
 end
-
